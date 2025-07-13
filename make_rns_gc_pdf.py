4c28c3154dd53a457f9c887c1dbe7246ed95cf27
Aaron Geller <aaron.s.geller@gmail.com>
2025-07-13 14:56:46 -0600

    Use power TFS and thresholded GC.

--- make_rns_gc_pdf.py
+++ make_rns_gc_pdf.py
@@ -12,6 +12,7 @@ figspath_forwardthresh = os.path.join(figspath, "forward_thresh")
 figspath_backward = os.path.join(figspath, "backward")
 figspath_backwardthresh = os.path.join(figspath, "backward_thresh")
 figspath_diff = os.path.join(figspath, "diff")
+figspath_powtfs = os.path.join(figspath, "powtfs")
 
 texfname = os.path.join("analyses", subjname, subjname + "_" + topdir + ".tex")
 texf = open(texfname, 'w')
@@ -26,13 +27,13 @@ preamble = "\\let\\mypdfximage\\pdfximage\n\\def\\pdfximage{\\immediate\\mypdfxi
     + "\\hypersetup{colorlinks=true,linkcolor=blue,pdftitle={" + title + "}}\n" \
     + "\\renewcommand{\\familydefault}{\\sfdefault}\n\\title{" + title + "}\n" \
     + "\\begin{document}\n\\maketitle\clearpage\pagenumbering{gobble}\n\n" \
-    + "\\setlength\\LTleft{-40mm}\\begin{landscape}\n\\hspace{-4cm}\n\\begin{longtable}{|c|c|c|c|c|}\n" \
+    + "\\setlength\\LTleft{-40mm}\\begin{landscape}\n\\hspace{-4cm}\n\\begin{longtable}{|c|c|c|c|c|c|}\n" \
     + "\caption{Granger causality plots.} \label{tab:long} \\\\\n\n" \
-    + "\hline \multicolumn{1}{|c|}{\\textbf{Channel pair}} & \multicolumn{1}{c|}{\\textbf{Forward GC}} & \multicolumn{1}{c|}{\\textbf{Backward GC}} & \multicolumn{1}{c|}{\\textbf{z(Forward GC)}} & \multicolumn{1}{c|}{\\textbf{z(Backward GC)}} \\\\ \hline\n" \
+    + "\hline \multicolumn{1}{|c|}{\\textbf{z(Ch 1 Power)}} & \multicolumn{1}{|c|}{\\textbf{z(Ch 2 Power)}}  & \multicolumn{1}{c|}{\\textbf{z(Forward GC)}} & \multicolumn{1}{c|}{\\textbf{z(Backward GC)}} & \multicolumn{1}{c|}{\\textbf{thresholded}} & \multicolumn{1}{c|}{\\textbf{thresholded}} \\\\ & & & & Forward GC & Backward GC \\\\ \hline\n" \
     + "\endfirsthead\n\n" \
-    + "\multicolumn{5}{c}%\n" \
+    + "\multicolumn{6}{c}%\n" \
     + "{{\\bfseries \\tablename\ \\thetable{} -- continued from previous page}} \\\\\n" \
-    + "\hline \multicolumn{1}{|c|}{\\textbf{Channel pair}} & \multicolumn{1}{c|}{\\textbf{Forward GC}} & \multicolumn{1}{c|}{\\textbf{Backward GC}} & \multicolumn{1}{c|}{\\textbf{z(Forward GC)}} & \multicolumn{1}{c|}{\\textbf{z(Backward GC)}} \\\\ \hline\n" \
+    + "\hline \multicolumn{1}{|c|}{\\textbf{z(Ch 1 Power)}} & \multicolumn{1}{|c|}{\\textbf{z(Ch 2 Power)}}  & \multicolumn{1}{c|}{\\textbf{z(Forward GC)}} & \multicolumn{1}{c|}{\\textbf{z(Backward GC)}} & \multicolumn{1}{c|}{\\textbf{thresholded}} & \multicolumn{1}{c|}{\\textbf{thresholded}} \\\\ & & & & Forward GC & Backward GC \\\\ \hline\n" \
     + "\endhead\n\n" \
     + "\hline \multicolumn{5}{|r|}{{Continued on next page}} \\\\ \hline\n" \
     + "\endfoot\n\n" \
@@ -41,11 +42,21 @@ preamble = "\\let\\mypdfximage\\pdfximage\n\\def\\pdfximage{\\immediate\\mypdfxi
 
 texf.write(preamble)
 
-imgfiles_forward = glob(os.path.join(figspath_forward, '*.png'))
+imgfiles_forward = glob(os.path.join(figspath_forward, 'z*.png'))
 imgfiles_forward.sort()
 
-imgfiles_backward = glob(os.path.join(figspath_backward, '*.png'))
+imgfiles_backward = glob(os.path.join(figspath_backward, 'z*.png'))
 imgfiles_backward.sort()
+
+imgfiles_powtfs = glob(os.path.join(figspath_powtfs, 'z*.png'))
+imgfiles_powtfs.sort()
+
+thresh_forward = glob(os.path.join(figspath_forward, 'thresh*.png'))
+thresh_forward.sort()
+
+thresh_backward = glob(os.path.join(figspath_backward, 'thresh*.png'))
+thresh_backward.sort()
+
 
 # imgfiles_diff = glob(os.path.join(figspath_diff, '*.png'))
 # imgfiles_diff.sort()
@@ -55,31 +66,37 @@ imgfiles_backward.sort()
 # imgfiles_thresh_backward = glob(os.path.join(figspath_backwardthresh, '*.png'))
 # imgfiles_thresh_backward.sort()
 
-imgsize = 0.37
+imgsize = 0.25
 clusterpics = 6
 #total_pairs = int((len(imgfiles_forward)-clusterpics)/2)
 total_pairs = int((len(imgfiles_forward))/2)
 
 imgsize2 = 0.5
 
+pairs = [[0,1],[0,2],[0,3],[1,2],[1,3],[2,3]]
+
 for i,f in enumerate(imgfiles_forward):
     fparts = os.path.basename(f).split('_')
-    if fparts[0]=="z" or fparts[0]=='thresh':
-        break
-    elif fparts[0]=="400":
+    if fparts[0]=="400":
         texf.write("\pagebreak\n")
     seedstr = fparts[1]
     target = fparts[2][:-4]
     label = seedstr + " $\\leftrightarrow$\\\\ " + target
-    thisline = "\\begin{tabular}{c}\n" + label + " \n\end{tabular}\n&\n" \
+    pow1f = imgfiles_powtfs[pairs[i][0]]
+    pow2f = imgfiles_powtfs[pairs[i][1]]
+    thisline = \
+        "\\begin{tabular}{c}\n\\includegraphics[width=" \
+        + str(imgsize) + "\\textwidth]{" + pow1f + "}\n\end{tabular}\n&\n" \
+        "\\begin{tabular}{c}\n\\includegraphics[width=" \
+        + str(imgsize) + "\\textwidth]{" + pow2f + "}\n\end{tabular}\n&\n" \
         + "\\begin{tabular}{c}\n\\includegraphics[width=" \
-        + str(imgsize) + "\\textwidth]{" + f + "}\n\end{tabular}\n&\n" \
+        + str(imgsize) + "\\textwidth]{" + imgfiles_forward[i] + "}\n\end{tabular}\n&\n" \
         + "\\begin{tabular}{c}\n\\includegraphics[width=" \
         + str(imgsize) + "\\textwidth]{" + imgfiles_backward[i] + "}\n\end{tabular}\n&\n" \
         + "\\begin{tabular}{c}\n\\includegraphics[width=" \
-        + str(imgsize) + "\\textwidth]{" + imgfiles_forward[i+total_pairs] + "}\n\end{tabular}\n&\n" \
+        + str(imgsize) + "\\textwidth]{" + thresh_forward[i] + "}\n\end{tabular}\n&\n" \
         + "\\begin{tabular}{c}\n\\includegraphics[width=" \
-        + str(imgsize) + "\\textwidth]{" + imgfiles_backward[i+total_pairs] + "}\n\end{tabular}\n\\\\\n"
+        + str(imgsize) + "\\textwidth]{" + thresh_backward[i] + "}\n\end{tabular}\n\\\\\n"
     texf.write(thisline)
 
 postamble = "\\end{longtable}\n"
